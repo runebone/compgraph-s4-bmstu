793af308a4e9a1d73864f252eefd5a8a718f77a3
Konstantin Runov <runebone1@gmail.com>
2023-03-02 20:31:14 +0300

    mw: rm comments; graphicsView -> MyGraphicsView

--- lab1/lab1/mainwindow.cpp
+++ lab1/lab1/mainwindow.cpp
@@ -1,8 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-#include <QFileDialog>
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -25,57 +23,6 @@ MainWindow::MainWindow(QWidget *parent)
     connect(this, SIGNAL(load_from_file_button_clicked(QString)), controller, SLOT(on_load_from_file_button_clicked(QString)));
     connect(this, SIGNAL(back_button_clicked()), controller, SLOT(on_back_button_clicked()));
     connect(this, SIGNAL(clear_screen()), controller, SLOT(on_clear_screen_clicked()));
-
-    // Let the View know if user presses any keys
-    // (I should've initially created custom QGraphicsView...)
-    connect(this, SIGNAL(key_pressed(QKeyEvent*)), view, SLOT(key_press_event(QKeyEvent*)));
-
-    // Tmp
-    double sqrt3 = std::sqrt(3);
-
-    std::vector<Point> pa{
-        // { .x = 0.0, .y = 1.0 }, // default
-        // { .x = -sqrt3/2, .y = -1.0/2 },
-        // { .x = sqrt3/2, .y = -1.0/2 },
-        // { .x = 0.0, .y = 1.0 * 10 }, // big
-        // { .x = -sqrt3/2 * 10, .y = -1.0/2 * 10 },
-        // { .x = sqrt3/2 * 10, .y = -1.0/2 * 10 },
-    };
-
-    std::vector<Point> pb{
-        // { .x = sqrt3/2, .y = 1.5 }, // FIXED: error sorting intersection points
-        // { .x = 0.0, .y = 0.0 },
-        // { .x = 2*sqrt3/2, .y = 0.0 },
-        // { .x = sqrt3/2 - 0.5, .y = 1.5 + 0.2}, // FIXED: error sorting intersection points
-        // { .x = 0.0 - 0.5, .y = 0.0 + 0.2},
-        // { .x = 2*sqrt3/2 - 0.5, .y = 0.0 + 0.2},
-        // { .x = sqrt3/2 + 0.0, .y = 1.5 + 1.0}, // share common side, no intersection area
-        // { .x = 0.0 + 0.0, .y = 0.0 + 1.0},
-        // { .x = 2*sqrt3/2 + 0.0, .y = 0.0 + 1.0},
-        // { .x = 0.0, .y = 1.0 + 1.999 }, // very small intersection area; ok
-        // { .x = -sqrt3/2, .y = -1.0/2 + 1.999 },
-        // { .x = sqrt3/2, .y = -1.0/2 + 1.999 },
-        // { .x = 0.0 + 0.2, .y = 1.0 + 1.000 }, // FIXED: error: raycast horizontal ray goes right through the vertex -> counts twice
-        // { .x = -sqrt3/2 + 0.2, .y = -1.0/2 + 1.000 },
-        // { .x = sqrt3/2 + 0.2, .y = -1.0/2 + 1.000 },
-        // { .x = 0.0 + 0.0, .y = 1.0 + 2.000 }, // share common point
-        // { .x = -sqrt3/2 + 0.0, .y = -1.0/2 + 2.000 },
-        // { .x = sqrt3/2 + 0.0, .y = -1.0/2 + 2.000 },
-        // { .x = 0.0 + 0.2, .y = 1.0 + 2.800 }, // no intersection
-        // { .x = -sqrt3/2 + 0.2, .y = -1.0/2 + 2.800 },
-        // { .x = sqrt3/2 + 0.2, .y = -1.0/2 + 2.800 },
-    };
-
-    // for (Point p: pa) {
-        // std::printf("1 %lf %lf\n", p.x, p.y);
-    // }
-
-    // for (Point p: pb) {
-        // std::printf("2 %lf %lf\n", p.x, p.y);
-    // }
-
-    // model->set_first_set_points(pa);
-    // model->set_second_set_points(pb);
 }
 
 MainWindow::~MainWindow()
@@ -96,12 +43,6 @@ void MainWindow::on_solveButton_clicked()
     emit this->solve_button_clicked();
 }
 
-void MainWindow::keyPressEvent(QKeyEvent *event)
-{
-    emit this->key_pressed(event);
-}
-
-
 void MainWindow::on_exitButton_clicked()
 {
     this->close();
@@ -118,11 +59,11 @@ void MainWindow::on_addPoint2Button_clicked()
     emit this->add2_button_clicked();
 }
 
+#include <QFileDialog>
 void MainWindow::on_loadFromFileButton_clicked()
 {
     // Should have been entirely in Controller
     QString filename = QFileDialog::getOpenFileName(this, tr("Open File"), "/home/human/University/cg/lab1", "Text File (*.txt)");
-
     emit this->load_from_file_button_clicked(filename);
 }
 
@@ -137,4 +78,3 @@ void MainWindow::on_actionClear_Screen_triggered()
 {
     emit this->clear_screen();
 }
-
--- lab1/lab1/mainwindow.h
+++ lab1/lab1/mainwindow.h
@@ -3,7 +3,7 @@
 
 #include <QMainWindow>
 
-#include "controller.h"
+#include "old_controller.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -25,30 +25,18 @@ signals:
     void back_button_clicked();
     void clear_screen();
 
-    void key_pressed(QKeyEvent *event);
-
 private slots:
-
     void on_actionTask_triggered();
-
     void on_solveButton_clicked();
-
     void on_exitButton_clicked();
-
     void on_addPoint1Button_clicked();
-
     void on_addPoint2Button_clicked();
-
     void on_loadFromFileButton_clicked();
-
     void on_backButton_clicked();
-
     void on_actionClear_Screen_triggered();
 
 private:
     Ui::MainWindow *ui;
     Controller *controller;
-
-    void keyPressEvent(QKeyEvent *event) override;
 };
 #endif // MAINWINDOW_H
