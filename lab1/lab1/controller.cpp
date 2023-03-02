da1c02931072ae6be1b9924d8f32e67b6a78f029
Konstantin Runov <runebone1@gmail.com>
2023-03-02 21:56:24 +0300

    C: added basic signals and slots

--- lab1/lab1/controller.cpp
+++ lab1/lab1/controller.cpp
@@ -6,9 +6,9 @@ Controller::Controller(Model *model, View *view, QObject *parent)
     m_view = view;
     m_history = new History;
 
-    connect(view->m_view, SIGNAL(leftMouseClicked(QMouseEvent*)), this, SLOT());
-    connect(view->m_view, SIGNAL(rightMouseClicked(QMouseEvent*)), this, SLOT());
-    connect(view->m_view, SIGNAL(keyPressed(QKeyEvent*)), this, SLOT());
+    connect(view->m_view, SIGNAL(leftMouseClicked(QMouseEvent*)), this, SLOT(on_left_mouse_clicked(QMouseEvent*)));
+    connect(view->m_view, SIGNAL(rightMouseClicked(QMouseEvent*)), this, SLOT(on_right_mouse_clicked(QMouseEvent*)));
+    connect(view->m_view, SIGNAL(keyPressed(QKeyEvent*)), this, SLOT(on_key_pressed(QKeyEvent*)));
 
     connect(model, SIGNAL(updated()), view, SLOT());
 }
@@ -20,4 +20,49 @@ Controller::~Controller()
     // XXX: In this program controller owns model and view
     delete m_model;
     delete m_view;
+}
+
+void Controller::on_solve_button_clicked()
+{
+    emit solve_button_clicked();
+}
+
+void Controller::on_add_button_1_clicked()
+{
+    emit add_button_1_clicked();
+}
+
+void Controller::on_add_button_2_clicked()
+{
+    emit add_button_2_clicked();
+}
+
+void Controller::on_load_from_file_button_clicked(QString)
+{
+    // TODO
+}
+
+void Controller::on_back_button_clicked()
+{
+    // TODO
+}
+
+void Controller::on_clear_screen_clicked()
+{
+    emit clear_screen_clicked();
+}
+
+void Controller::on_key_pressed(QKeyEvent *event)
+{
+    emit key_pressed(event);
+}
+
+void Controller::on_left_mouse_clicked(QMouseEvent *event)
+{
+    emit left_mouse_clicked(event);
+}
+
+void Controller::on_right_mouse_clicked(QMouseEvent *event)
+{
+    emit right_mouse_clicked(event);
 }
--- lab1/lab1/controller.h
+++ lab1/lab1/controller.h
@@ -19,9 +19,28 @@ private:
     History *m_history;
 
 public slots:
+    void on_solve_button_clicked();
+    void on_add_button_1_clicked();
+    void on_add_button_2_clicked();
+    void on_load_from_file_button_clicked(QString);
+    void on_back_button_clicked();
+    void on_clear_screen_clicked();
+
+    void on_key_pressed(QKeyEvent*);
+    void on_left_mouse_clicked(QMouseEvent*);
+    void on_right_mouse_clicked(QMouseEvent*);
 
 signals:
+    void solve_button_clicked();
+    void add_button_1_clicked();
+    void add_button_2_clicked();
+    // void load_from_file_button_clicked(QString);
+    // void back_button_clicked();
+    void clear_screen_clicked();
 
+    void key_pressed(QKeyEvent*);
+    void left_mouse_clicked(QMouseEvent*);
+    void right_mouse_clicked(QMouseEvent*);
 };
 
 #endif // CONTROLLER_H
