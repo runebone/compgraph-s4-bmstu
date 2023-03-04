a1f84da0a0dc6191944674a48092acf9361162aa
Konstantin Runov <runebone1@gmail.com>
2023-03-04 19:05:44 +0300

    V: fixes, basic signals and slots, a lot of code.

--- lab1/lab1/view.cpp
+++ lab1/lab1/view.cpp
@@ -16,8 +16,100 @@ View::View(MyGraphicsView *graphicsView, QScrollArea *leftScrollArea, QScrollAre
     // Binding layouts to ScrollAreas
     QWidget *lWidget = new QWidget(leftScrollArea);
     QWidget *rWidget = new QWidget(rightScrollArea);
-    lWwidget->setLayout(m_leftLayout);
-    rWwidget->setLayout(m_rightLayout);
+    lWidget->setLayout(m_leftLayout);
+    rWidget->setLayout(m_rightLayout);
     leftScrollArea->setWidget(lWidget);
     rightScrollArea->setWidget(rWidget);
+}
+
+void View::setModel(Model *model)
+{
+    m_model = model;
+}
+
+MyGraphicsView *View::getView() const
+{
+    return m_view;
+}
+
+void clearLayout(QLayout *layout) {
+    QLayoutItem* item;
+    QWidget* widget;
+
+    while ((item = layout->takeAt(0)) != nullptr) {
+        widget = item->widget();
+        if (widget != nullptr) {
+            delete widget;
+        }
+        delete item;
+    }
+}
+
+void View::addMyPointWidgetToLayout(QLayout *layout, MyPointWidget *mpw) {
+    layout->addWidget(mpw);
+
+    connect(mpw, SIGNAL(x_input_changed(size_t,double,Set)), this, SLOT(on_point_x_changed(size_t,double,Set)));
+    connect(mpw, SIGNAL(y_input_changed(size_t,double,Set)), this, SLOT(on_point_y_changed(size_t,double,Set)));
+    connect(mpw, SIGNAL(remove_button_clicked(size_t,Set)), this, SLOT(on_point_remove_clicked(size_t,Set)));
+
+    if (mpw->set() == FIRST) {
+        connect(mpw, SIGNAL(invalid_input(size_t)), this, SLOT(on_invalid_input_left(size_t)));
+    } else if (mpw->set() == SECOND) {
+        connect(mpw, SIGNAL(invalid_input(size_t)), this, SLOT(on_invalid_input_right(size_t)));
+    }
+}
+
+void View::addPointsToLayout(QLayout *layout, std::vector<Point> &points, Set set) {
+    int i = 0;
+    for (Point p: points) {
+        MyPointWidget *mpw = new MyPointWidget(i, p);
+        mpw->set_set(set);
+        addMyPointWidgetToLayout(layout, mpw);
+        i++;
+    }
+}
+
+void View::on_model_updated(ModelData md)
+{
+    clearLayout(m_leftLayout);
+    clearLayout(m_rightLayout);
+
+    addPointsToLayout(m_leftLayout, md.firstSetPoints, FIRST);
+    addPointsToLayout(m_rightLayout, md.secondSetPoints, SECOND);
+
+    // TODO
+}
+
+void View::on_clear_screen_clicked()
+{
+    m_model->dbg_emit_updated();
+}
+
+void View::on_point_x_changed(size_t index, double value, Set set)
+{
+    Point p = m_model->get_point(index, set);
+    p.x = value;
+    m_model->update_point(index, p, set);
+}
+
+void View::on_point_y_changed(size_t index, double value, Set set)
+{
+    Point p = m_model->get_point(index, set);
+    p.y = value;
+    m_model->update_point(index, p, set);
+}
+
+void View::on_point_remove_clicked(size_t index, Set set)
+{
+    m_model->remove_point(index, set);
+}
+
+void View::on_invalid_input_left(size_t index)
+{
+    // TODO
+}
+
+void View::on_invalid_input_right(size_t index)
+{
+    // TODO
 }
--- lab1/lab1/view.h
+++ lab1/lab1/view.h
@@ -1,12 +1,15 @@
 #ifndef VIEW_H
 #define VIEW_H
 
+#include "model.h"
+
 #include <QObject>
 #include <QGraphicsView>
 #include <QScrollArea>
 #include <QLabel>
 #include <QVBoxLayout>
 
+#include "mypointwidget.h"
 #include "mygraphicsview.h"
 #include "mygraphicsscene.h"
 
@@ -15,6 +18,12 @@ class View : public QObject
     Q_OBJECT
 public:
     explicit View(MyGraphicsView *graphicsView, QScrollArea *leftScrollArea, QScrollArea *rightScrollArea, QLabel *infoLabel, QObject *parent = nullptr);
+
+    // XXX: Just for connecting the signals of MyPointWidgets
+    void setModel(Model *model);
+
+    // Just for Controller to connect some signals to the view
+    MyGraphicsView *getView() const;
 
 private:
     MyGraphicsView *m_view;
@@ -25,14 +34,29 @@ private:
 
     QLabel *m_infoLabel;
 
+    // XXX: Just for connecting the signals of MyPointWidgets
+    Model *m_model;
+
+    // XXX: Ugly way
+    void addMyPointWidgetToLayout(QLayout *layout, MyPointWidget *mpw);
+    void addPointsToLayout(QLayout *layout, std::vector<Point> &points, Set set);
+
 public slots:
-    void on_model_updated();
+    void on_model_updated(ModelData);
 
     void on_clear_screen_clicked();
 
-    void on_key_pressed(QKeyEvent*);
-    void on_left_mouse_clicked(QMouseEvent*);
-    void on_right_mouse_clicked(QMouseEvent*);
+    // void on_key_pressed(QKeyEvent*);
+    // void on_left_mouse_clicked(QMouseEvent*);
+    // void on_right_mouse_clicked(QMouseEvent*);
+
+private slots:
+    // XXX: Ugly way
+    void on_point_x_changed(size_t, double, Set);
+    void on_point_y_changed(size_t, double, Set);
+    void on_point_remove_clicked(size_t, Set);
+    void on_invalid_input_left(size_t);
+    void on_invalid_input_right(size_t);
 
 signals:
 
